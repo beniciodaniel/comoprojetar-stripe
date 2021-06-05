d8971c0d4b17313b27772b86fad4e39bb49b3fe8
Benício Barreto <benicio.daniel@gmail.com>
2021-06-05 10:16:33 -0300

    Adicionou lógica de NextAuth com Github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,30 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signin, useSession, signOut } from 'next-auth/client';
+
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLogged = true;
+  const [session] = useSession();
 
-  return isUserLogged ? (
-    <button type="button" className={styles.signInButton}>
+  console.log(session, 'SESSION');
+
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Benício Daniel
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signin('github')}
+    >
       <FaGithub color="#eba417" />
       Sign in with GitHub
     </button>
