dab8ab54d304c65ddda078a86505cd9cb00d7d43
Benício Barreto <benicio.daniel@gmail.com>
2021-06-28 19:28:39 -0300

    displaying prismic post using prismic dom

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,11 +1,22 @@
 import { GetStaticProps } from 'next';
 import { getPrismicClient } from '../../services/prismic';
+import { RichText } from 'prismic-dom';
 import Head from 'next/head';
 import Prismic from '@prismicio/client';
 import React from 'react';
 import styles from './styles.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -14,30 +25,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Arquitetura moderna e etc bla bla bla</strong>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro
-              consectetur sit quam odio a asperiores rerum saepe.
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Arquitetura moderna e etc bla bla bla</strong>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro
-              consectetur sit quam odio a asperiores rerum saepe.
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Arquitetura moderna e etc bla bla bla</strong>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro
-              consectetur sit quam odio a asperiores rerum saepe.
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -57,7 +51,27 @@ export const getStaticProps: GetStaticProps = async () => {
 
   console.log(JSON.stringify(response, null, 2));
 
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === 'paragraph')
+          ?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        }
+      )
+    };
+  });
+
   return {
-    props: {}
+    props: {
+      posts
+    }
   };
 };
