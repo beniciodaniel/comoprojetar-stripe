477a307baee6ecfedb30aee3cdf766ea3b7f2dd5
Benício Barreto <benicio.daniel@gmail.com>
2021-07-01 21:51:52 -0300

    adding about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+import React from 'react';
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>Sobre | Como Projetar</title>
+      </Head>
+    </>
+  );
+}
